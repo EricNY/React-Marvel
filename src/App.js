d544c0c148698c1cffed69b3d2b24149250ded7f
EricNY <eric.abt@evercore.com>
2019-02-02 15:20:16 -0500

    add axios function to fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import md5 from 'md5';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+
+      const urlBaseMarvel = 'http://gateway.marvel.com/v1/public/';
+      const apiKey = 'a4175dba2a7fc9ccb00c6ac86098467d';
+      const ts = Date.now();
+      const privateKey = '90be868cb5a4f37fe5644a2e8a1619eee99c020a';
+      const hash = md5(ts + privateKey + apiKey);
+      const url = urlBaseMarvel + 'comics?apikey=' +  apiKey + '&hash=' + hash + '&ts=' + ts;
+
+      axios.get(url).then((comics) => {
+	  console.log(comics.data.data.results);
+      }).catch(e => console.log(e));
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+	 <p>React Marvel</p>
       </div>
     );
   }
